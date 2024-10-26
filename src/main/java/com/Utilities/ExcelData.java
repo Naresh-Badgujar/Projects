f7a01c1f89433992e1df7389369ee1d81a8811c7
unknown <nareshbadgujar01@gmail.com>
2024-10-26 17:13:40 +0530

    My Project commit

---
+++ src/main/java/com/Utilities/ExcelData.java
@@ -0,0 +1,13 @@
+package com.Utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExcelData {
+	
+	public void ExcelFileRead(String filepath) throws FileNotFoundException {
+		FileInputStream fis = new FileInputStream(filepath);
+
+	}
+
+}
